6a8d037c519b82bcf3224fbe10be2b97cd9a7139
Jesus Magana <maganazero5@gmail.com>
2014-07-06 09:32:33 -0700

    Initial Commit

---
+++ BreakoutSpriteKitTutorial/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BreakoutSpriteKitTutorial
+//
+//  Created by Jesus Magana on 7/6/14.
+//  Copyright (c) 2014 ZeroLinux5. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BreakoutSpriteKitTutorial/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  BreakoutSpriteKitTutorial
+//
+
+//  Copyright (c) 2014 ZeroLinux5. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+
+@interface ViewController : UIViewController
+
+@end
